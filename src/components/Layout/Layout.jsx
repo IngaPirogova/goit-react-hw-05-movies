fb20e7840914e26bf8681f4b3dc32e3b0354440a
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-02-02 12:58:58 +0200

    Summary

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,9 @@
+
+export const Layout = () => {
+    return (
+       < div display="grid" gridTemplateColumns="200px 1fr">
+           <div>Home</div>
+           <div>Invoices</div>
+       </div>
+    )
+}
