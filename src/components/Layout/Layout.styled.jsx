aaf5c8d84317764ad914600b959ef8a792780cbb
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-02-03 00:44:17 +0200

    upd

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const StyledLink = styled(NavLink)`
     color: blue;
-    text-decoration: none;
+    list-style: none;
 
 &.active {
   color: orange;
