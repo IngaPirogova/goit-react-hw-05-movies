d7306e7fca42a7d6daa3e73d48fe425eed40ca25
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-02-04 12:30:53 +0200

    Summary

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,36 @@
-import axios from "axios";
-
-axios.defaults.baseURL = 'https://app.ticketmaster.com/discovery/v2/';
-const API_KEY = 'tIj1kC332ExvV8vs1uBAp1fasaO5ERpG';
 
 
-export async function fetchEvents() {
-    const res = await axios('events', { params: { apikey: API_KEY}});
-    return res.data._embedded.events;
-}
+//key 645002417e61fac3d7b9bfd937ca50a9
 
-export async function fetchEventsById(id) {
-    const res = await axios(`events/${id}`, { params: { apikey: API_KEY}});
-    return res.data;
-}
+// https://www.themoviedb.org/settings/api
+
+
+// Пример API-запроса  https://api.themoviedb.org/3/movie/550?api_key=645002417e61fac3d7b9bfd937ca50a9
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import axios from "axios";
+
+// axios.defaults.baseURL = 'https://app.ticketmaster.com/discovery/v2/';
+// const API_KEY = 'tIj1kC332ExvV8vs1uBAp1fasaO5ERpG';
+
+
+// export async function fetchEvents() {
+//     const res = await axios('events', { params: { apikey: API_KEY}});
+//     return res.data._embedded.events;
+// }
+
+// export async function fetchEventsById(id) {
+//     const res = await axios(`events/${id}`, { params: { apikey: API_KEY}});
+//     return res.data;
+// }
