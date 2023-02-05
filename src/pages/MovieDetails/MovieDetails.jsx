3ef2fcc39670f88b50d1053e43dec50e82a03459
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-02-05 21:29:08 +0200

    upd

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,22 +1,16 @@
- import { useFetchMovie } from 'hooks/useFetchMovie';
+import { useFetchMovie } from 'hooks/useFetchMovie';
 //  import { Link } from 'react-router-dom';
 
 export const MovieDetails = () => {
   const movie = useFetchMovie();
   return (
     movie && (
-        <div>
+      <div>
         <h2>{movie.title}</h2>
         <img src={movie.poster_path} alt={movie.title} />
         <p>{movie.overview}</p>
         <p>{movie.genre}</p>
-
-    
-        </div>
+      </div>
     )
-  )
-    
-    
-  
- 
-}
+  );
+};
