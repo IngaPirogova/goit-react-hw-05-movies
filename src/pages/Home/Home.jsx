b0b07c1444a555076a3c3d080f567e93f31365d2
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-02-05 21:05:01 +0200

    upd

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,7 +17,7 @@ export const Home = () => {
       <ul>
         {movies.map(({ id, title, name }) => (
           <li key={id}>
-            <Link to={`movies/${id}`}>{title}{name}</Link>                      
+            <Link to={`/movies/${id}`}>{title}{name}</Link>                      
           </li>
         ))}
       </ul>
