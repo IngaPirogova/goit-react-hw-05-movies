e128b41a889322df4de1a7aee4fe409369c09a14
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-02-07 22:37:51 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <BrowserRouter>
+   <BrowserRouter basename='/goit-react-hw-05-movies/'>
     <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
 
-//  <BrowserRouter basename='/goit-react-hw-05-movies/'>тзтз
+//  <BrowserRouter basename='/goit-react-hw-05-movies/'>
